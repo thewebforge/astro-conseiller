7a716075aecfcae7d57a42eedb02f43f9db360ad
Cedric <cedric@oxyprops.com>
2023-04-26 17:29:12 +0200

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://cedric.vosconseillers.fr",
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), mdx(), sitemap(), robotsTxt()]
+});
